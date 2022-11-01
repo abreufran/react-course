e49be8fabf1364f05ab27ca8e3631976ae07dae1
Francisco Abreu <francisco.abreu@externos-cl.cencosud.com>
2022-11-01 18:54:28 -0300

    Changes.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Button } from "./Button";
 import { Greeting, UserCard } from "./Greeting";
@@ -13,8 +13,78 @@ const handleChange = (e) => {
   console.log(e.target.value);
 };
 
+function Counter() {
+  const [counter, setCounter] = useState(10);
+
+  return (
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Sumar</button>
+    </div>
+  );
+}
+
+function InputCounter() {
+  const [mensaje, setMensaje] = useState("");
+
+  const [inputCounter, setInputCounter] = useState(0);
+
+  //Cuando el useEffect tiene como segundo parametro un array vacio se ejecuta una vez
+  //Cuando no tienen segundo parámetro se ejecuta siempre
+  //Cuando dentro del arreglo esta una variable se ejecuta cada vez que cambia la variable.
+  useEffect(
+    function () {
+      console.log("render");
+    },
+    [inputCounter]
+  );
+
+  return (
+    <div>
+      <input onChange={(e) => setMensaje(e.target.value)} />
+      <button onClick={(e) => alert("El mensaje es: " + mensaje)}>Save</button>
+
+      <hr />
+      <h1>Counter: {inputCounter}</h1>
+      <button onClick={() => setInputCounter(inputCounter + 1)}>
+        Incrementar
+      </button>
+
+      <Counter />
+    </div>
+  );
+}
+
+const users = [
+  {
+    id: 1,
+    name: "Ryan ray",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+];
+
 root.render(
   <>
+    <InputCounter />
+
+    {users.map((u, i) => {
+      return <h1 key={i}>{u.name}</h1>;
+    })}
+
+    {users.map((u, i) => {
+      return (
+        <div key={i}>
+          <h1>{u.name}</h1>
+          <img src={u.image} />
+        </div>
+      );
+    })}
+
     <Greeting title="hola" salary={5} />
     <Greeting title="hola2" salary={10} />
 
